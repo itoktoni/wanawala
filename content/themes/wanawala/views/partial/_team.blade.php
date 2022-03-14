cf950b7fbd58065edb5b085e1773a2c521a24a94
itoktoni <itok.toni@gmail.com>
2022-03-14 13:30:20 +0700

    update tim kami

--- content/themes/wanawala/views/partial/_team.blade.php
+++ content/themes/wanawala/views/partial/_team.blade.php
@@ -9,7 +9,7 @@
     @if(!empty($data->team_list))
     <ul class="team-list">
         @foreach($data->team_list as $list_team)
-        <li>
+        <li class="flex-1">
             <img src="{{ $list_team->team_photo ?? '' }}" alt="{{ $list_team->team_name ?? '' }}">
             <h3>{{ $list_team->team_name ?? '' }}</h3>
             <h5>{{ $list_team->team_jabatan ?? '' }}</h5>
